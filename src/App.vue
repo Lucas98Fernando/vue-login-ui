b009bab7c433657609626ee08ba2e3de1bfe4cb8
Lucas Fernando <lucas98fernando@gmail.com>
2021-02-08 16:23:40 -0300

    Background changes

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,16 @@ export default {
   display: flex;
   align-items: center;
   min-height: 100vh;
-  background: linear-gradient(to right, #41b883, #83c5be);
+  /* background: linear-gradient(to right, #41b883, #83c5be); */
+  /* background: url('./assets/images/bg.jpg'); */
+  background: linear-gradient(
+      to bottom,
+      rgba(65, 184, 131, 0.7) 0%,
+      rgba(131, 197, 190, 0.7) 100%
+    ),
+    url("./assets/images/bg.jpg");
+  background-position: center;
+  background-size: cover;
   padding: 30px 0 0;
 }
 </style>
